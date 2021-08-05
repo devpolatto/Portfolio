dd5090d7ce9209e391688ffdb65b8cfe7083ca4e
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-08-05 18:00:26 -0300

    remove styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,11 +46,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;0,800;0,900;1,300;1,400;1,500;1,700;1,800;1,900&display=swap" rel="stylesheet" />
 
-                    <style>{`
-                        #__next { width: 100% }
-                    `}
-                    </style>
-
                 </Head>
                 <body>
                     <Main />
