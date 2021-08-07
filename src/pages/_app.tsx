685a1dd67680f4ee1645c565c4e3558d8c1228f8
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-08-07 12:08:20 -0300

    Finished Remove CSSThem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,21 @@
-import { useState } from 'react';
 import Head from 'next/head'
 import type { AppProps } from 'next/app';
 
-import { ThemeProvider } from 'styled-components';
-
 import Header from '../components/Header';
-
-import dark from '../../styles/themes/dark';
-import light from '../../styles/themes/light';
 
 import { GlobalStyled } from '../../styles/Global';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const [theme, setTheme] = useState(dark)
-
-  function handleSetTheme() {
-    if (theme.title === 'dark') {
-      setTheme(light)
-    } else (
-      setTheme(dark)
-    )
-  }
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Head>
         <title>Polatto.dev</title>
       </Head>
       <Header/>
       <Component {...pageProps} />
       <GlobalStyled />
-    </ThemeProvider>
+    </>
   )
 
 }
