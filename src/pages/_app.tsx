420373bec2d3ca1892865aa2b0e9090df7189dd5
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-08-13 21:17:36 -0300

    add Effect Load Transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
 import Head from 'next/head'
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
+import NProgress from 'nprogress';
 
 import Header from '../components/Header';
 
 import { GlobalStyled } from '../../styles/Global';
+import '../../styles/nprogress.css';
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading ${url}`);
+  NProgress.start();
+})
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done()
+})
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done()
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
 
