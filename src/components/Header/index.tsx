1677e643bcfd40c1213f803b939b08e77a8d15fe
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-10-05 18:29:44 -0300

    add Header efect slide

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ReactNode } from 'react';
 import { FaSun, FaMoon, FaBars } from 'react-icons/fa'
+import { HiX } from "react-icons/hi"
 import {useRouter} from 'next/router'
 
 import { 
-    Heading, 
     Flex, 
     Box,
     Container,
     Link,
     useMediaQuery,
-    Divider,
     Text,
     Collapse,
-    SlideFade,
-    useDisclosure
+    useDisclosure,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import { useColorMode } from "@chakra-ui/color-mode";
-import { HStack } from '@chakra-ui/layout';
+import { HStack, Stack } from '@chakra-ui/layout';
 import { IconButton} from '@chakra-ui/button'
+
+
+const Links = ['Dashboard', 'Projects', 'Team'];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+    <Link
+      px={5}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={'#'}>
+      {children}
+    </Link>
+  );
 
 function Header() {
 
@@ -28,84 +44,68 @@ function Header() {
     const [isLargerThan1024px] = useMediaQuery("(min-width: 1024px)")
 
     const router = useRouter()
-    const isRouteHomeActive = router.pathname === '/'   
+    const isRouteHomeActive = router.pathname === '/'    
     const isRouteProjectsActive = router.pathname === '/projects'
     const isRouteAboutActive = router.pathname === '#about-me'
 
-    const { isOpen, onToggle } = useDisclosure()
+    const { isOpen, onToggle, onClose, onOpen } = useDisclosure()
+
+    function toggleMenuAfterChangeRoute(route: string){
+        onClose
+        router.push(route)
+    }
+
+    function close(){
+        () => onToggle
+    }
+
+    useEffect(() => {
+        console.log(isOpen)
+    },[isOpen])
 
     return (
-        <Container w="full" maxW="container.xl" py="4">
-            <Flex w="100%" align="center" justifyContent="space-between">
-                <Heading>
-                    Polatto<span>.</span>dev
-                </Heading>
-                
-                <Box w="auto" display="flex" alignItems="center" justifyContent="space-between">
-                    <Flex
-                        pos={isLargerThan1024px ? 'unset' : 'absolute'}
-                        top={isLargerThan1024px ? 'unset' : '75px'}
-                        left={isLargerThan1024px ? 'unset' : '0px'}
-                        w='full'
-                        
-                        align="center"  
-                        justifyContent={isLargerThan1024px ? 'space-between' : 'center'}
-                        px="1"
-                    >
-                        <SlideFade in={isOpen} offsetX={isLargerThan1024px ? '80px' : 'unset'} offsetY="0">
-                                             
-                            <HStack direction='row' spacing={14} mr={isLargerThan1024px ? 5 : 0}>
-                                <Link onClick={() => router.push('/')}>
-                                    <Text
-                                        color={isRouteHomeActive && isDark  ? '#7CFFC4' : colorMode}
-                                        fontSize={isLargerThan768px ? '2xl' : 'md'}
-                                        fontWeight="semibold" 
-                                        textTransform="uppercase"
-                                    >Inicio</Text>
-                                </Link>
-
-                                <Link 
-                                    color={isRouteAboutActive && isDark ? '#7CFFC4' : colorMode}
-                                    fontSize={isLargerThan768px ? '2xl' : 'md'}
-                                    fontWeight="semibold"
-                                    textTransform="uppercase"
-                                    textDecorationLine="none"
-                                    textDecoration="none"
-                                    onClick={() => router.push('/#about-me')}
-                                >   Sobre Mim
-                                </Link>
-
-                                <Link 
-                                    color={isRouteProjectsActive  && isDark ? '#7CFFC4' : colorMode}
-                                    fontSize={isLargerThan768px ? '2xl' : 'md'}
-                                    fontWeight="semibold"
-                                    textTransform="uppercase"
-                                    onClick={() => router.push('/projects')}
-                                > Projetos
-                                </Link>
-                                <IconButton 
-                                    fontSize={20}
-                                    icon={isDark  ? <FaSun color="#7CFFC4" /> : <FaMoon />} 
-                                    isRound={true} 
-                                    variant="outline"
-                                    arial-label="Change Theme"
-                                    onClick={toggleColorMode}/>
-                            </HStack>
-
-                        </SlideFade>
-                    </Flex>
-
-                    <IconButton 
-                        alignContent="center"
-                        justifyContent="center"
-                        fontSize={30}
-                        icon={<FaBars color={isDark ? '#F7FAFC' : '#0e0e0e'} />} 
-                        arial-label="Change Theme"
-                        onClick={onToggle}
-                    />
-                </Box>
-            </Flex>
-        </Container>
+        <Box w="full">
+            <Container w="full" maxW="container.xl">
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <HStack display="flex" spacing={8} alignItems={'center'} justifyContent="space-between">
+                        <Box>
+                            <Text fontSize="3xl">Polatto.dev</Text>
+                        </Box>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{ base: 'none', lg: 'flex' }}>
+                            {Links.map((link) => (
+                            <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </HStack>
+                    </HStack>
+                    {
+                        isLargerThan1024px ? '' : (
+                            <IconButton
+                                size={'lg'}
+                                icon={isOpen ? <HiX color="#9B2C2C" fontSize="28" /> : <FaBars color="#7CFFC4" fontSize="28" />}
+                                variant="ghost"
+                                colorScheme="#7CFFC4"
+                                aria-label={'Open Menu'}
+                                onClick={isOpen ? onClose : onOpen}
+                            />
+                        )
+                    }
+                </Flex>
+                {!isLargerThan1024px ? (
+                        <Collapse in={isOpen} startingHeight={0}>
+                            <Box w="full">
+                                <Stack as={'nav'} alignItems="center" spacing={3} w="full" bg={isLargerThan1024px ? '' : '#181C25'}>
+                                    {Links.map((link) => (
+                                    <NavLink key={link}>{link}</NavLink>
+                                    ))}
+                                </Stack>
+                            </Box>
+                        </Collapse>
+                ) : null}
+            </Container>
+        </Box>
     );
 }
 
